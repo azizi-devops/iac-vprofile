c17adf9b742e3baa8a0998149f5c20010c37b47a
azizi.devops <karim.azizi.master@gmail.com>
2025-08-04 20:10:54 +0200

    4343

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,7 +29,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-state-01.09"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 
   #required_version = "~> 1.5.1"
